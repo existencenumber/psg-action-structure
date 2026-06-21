44ddc5179a5028f04ab4091f5551911f70abd833
existencenumber <499913478@qq.com>
2026-06-21 10:23:21 +0800

    Create compute_all_constants.py

---
+++ examples/compute_all_constants.py
@@ -0,0 +1,17 @@
+"""
+Example: compute all physical constants and compare with experiment.
+
+Usage:
+    python examples/compute_all_constants.py
+"""
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
+import mpmath as mp
+mp.dps = 100
+
+from src.physics_constants import compute_all
+
+if __name__ == '__main__':
+    results = compute_all(verbose=True)
